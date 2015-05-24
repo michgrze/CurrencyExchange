c09b396e195aae0e3825925b037f93659496444f
ambisoft <mark@ambisoft.pl>
2015-05-24 10:55:17 +0200

    /exchange_rates JSON action - in progres

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'exchange_rates#index'
 
+  resources :exchange_rates, only: :index
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
